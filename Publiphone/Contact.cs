1cec1ee6be79363a44f863f01700a276302830e6
JuilletMikael <mikael.juillet@eduvaud.ch>
2023-12-19 15:27:13 +0100

    first

---
+++ Publiphone/Contact.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Publiphone
+{
+    public class Contact
+    {
+        public void call(Phone phone, float money, string number)
+        {
+            phone.AddMoney(money);
+            phone.Call(number);
+        }
+
+        public void endCall(Phone phone)
+        {
+            phone.EndCall();
+        }
+        
+    }
+}
---
+++ Publiphone/Phone.cs
@@ -0,0 +1,23 @@
+﻿namespace Publiphone
+{
+    public class Phone
+    {
+        private string phoneNumber = "0789938888";
+        private float money = 0;
+        
+        public void AddMoney(float money)
+        {
+            this.money += money;
+        }
+
+        public void Call(string number)
+        {
+            
+        }
+
+        public void EndCall()
+        {
+           
+        }
+    }
+}
